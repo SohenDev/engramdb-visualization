125eb58463f8d299ff35f2c2d76c1f93d57cd449
f <>
2023-06-25 14:33:31 +0800

    文字修改

--- pkg/api/index.go
+++ pkg/api/index.go
@@ -731,7 +731,7 @@ func (hs *HTTPServer) setIndexViewData(c *models.ReqContext) (*dtos.IndexViewDat
 		AppNameBodyClass:        "app-grafana",
 		FavIcon:                 "public/img/fav32.png",
 		AppleTouchIcon:          "public/img/apple-touch-icon.png",
-		AppTitle:                "Grafana",
+		AppTitle:                "数据可视化",
 		NavTree:                 navTree,
 		Sentry:                  &hs.Cfg.Sentry,
 		Nonce:                   c.RequestNonce,
